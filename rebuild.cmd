5f1b31608188cc0eee0f37dfb6e09b8485e2f7c8
Michael Sawczyn <michaelsawczyn@gmail.com>
2017-08-31 11:54:04 -0400

    Flipped column names in many-to-many mapping

--- rebuild.cmd
+++ rebuild.cmd
@@ -1,6 +1,6 @@
 cd src
-rem VSIXInstaller.exe /q /u:"56bbe1ba-aaee-4883-848f-e3c8656f8db2"
+VSIXInstaller.exe /q /u:"56bbe1ba-aaee-4883-848f-e3c8656f8db2"
 msbuild efdesigner.sln /t:Rebuild /p:Configuration=Debug
 msbuild efdesigner.sln /t:Rebuild /p:Configuration=Release
 copy /Y "DslPackage\bin\Release\Sawczyn.EFDesigner.EFModel.DslPackage.vsix" ..\dist
-rem VSIXInstaller.exe /q "DslPackage\bin\Release\Sawczyn.EFDesigner.EFModel.DslPackage.vsix"
+VSIXInstaller.exe /q "DslPackage\bin\Release\Sawczyn.EFDesigner.EFModel.DslPackage.vsix"
