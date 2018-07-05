ef2236b45a882f71a5939e194526d19783f21b9f
Michael Sawczyn <michaelsawczyn@gmail.com>
2018-07-05 08:29:42 -0400

    File drop continued

--- src/Dsl/CustomCode/Utilities/FileDropHelper.cs
+++ src/Dsl/CustomCode/Utilities/FileDropHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -59,94 +60,123 @@ namespace Sawczyn.EFDesigner.EFModel
       private static void ProcessClass(Store store, ClassDeclarationSyntax cls, NamespaceDeclarationSyntax ns = null)
       {
          ModelRoot modelRoot = store.ElementDirectory.AllElements.OfType<ModelRoot>().FirstOrDefault();
+         string className = cls.Identifier.Text;
 
+         #region Sanity checks
+
+         // can't add duplicate class names
+         if (store.ElementDirectory.AllElements.OfType<ModelClass>().Any(c => c.Name == className))
          {
-            string className = cls.Identifier.Text;
+            ErrorDisplay.Show($"'{className}' already exists in model.");
+            return;
+         }
 
-            //// can't add duplicate class names
-            //if (store.ElementDirectory.AllElements.OfType<ModelClass>().Any(c => c.Name == className))
-            //{
-            //   ErrorDisplay.Show($"'{className}' already exists in model.");
-            //   return;
-            //}
+         if (cls.TypeParameterList != null)
+         {
+            ErrorDisplay.Show($"Can't add generic class '{className}'.");
+            return;
+         }
 
-            if (cls.TypeParameterList != null)
+         #endregion
+
+         using (Transaction tx = store.TransactionManager.BeginTransaction("Process dropped class"))
+         {
+            ModelClass newClass = new ModelClass(store, new PropertyAssignment(ModelClass.NameDomainPropertyId, className));
+            modelRoot.Types.Add(newClass);
+
+            #region Namespace
+
+            if (ns != null)
             {
-               ErrorDisplay.Show($"Can't add generic class '{className}'.");
-               return;
+               string @namespace = ns.Members.OfType<QualifiedNameSyntax>().FirstOrDefault()?.ToString();
+
+               if (!string.IsNullOrEmpty(@namespace))
+                  newClass.Namespace = @namespace;
             }
 
-            using (Transaction tx = store.TransactionManager.BeginTransaction("Process dropped class"))
+            #endregion
+
+            if (cls.DescendantNodes().Any(n => n.Kind() == SyntaxKind.AbstractKeyword))
+               newClass.IsAbstract = true;
+
+            #region Base classes and interfaces
+
+            if (cls.BaseList != null)
             {
-               ModelClass newClass = new ModelClass(store, new PropertyAssignment(ModelClass.NameDomainPropertyId, className));
-               modelRoot.Types.Add(newClass);
+               List<string> customInterfaces = new List<string>();
 
-               if (ns != null)
+               foreach (BaseTypeSyntax type in cls.BaseList.Types)
                {
-                  string @namespace = ns.Members.OfType<QualifiedNameSyntax>().FirstOrDefault()?.ToString();
+                  string baseName = type.ToString();
 
-                  if (!string.IsNullOrEmpty(@namespace))
-                     newClass.Namespace = @namespace;
-               }
-
-               if (cls.DescendantNodes().Any(n => n.Kind() == SyntaxKind.AbstractKeyword))
-                  newClass.IsAbstract = true;
-
-               BaseTypeSyntax baseType = cls.BaseList?.Types.FirstOrDefault();
-
-               if (baseType != null)
-               {
-                  string baseName = baseType.ToString();
-                  newClass.Superclass = modelRoot.Types.OfType<ModelClass>().FirstOrDefault(c => c.Name == baseName);
-               }
-
-               // TODO: Continue here - separate base class from interfaces?
-
-               newClass.Attributes.Clear();
-               foreach (string source in cls.Members
-                                            .OfType<PropertyDeclarationSyntax>()
-                                            .Select(prop => prop.ToString())
-                                            .Select(source => Regex.Replace(source, @"\[[^]]+\]", "", RegexOptions.Multiline)
-                                                                   .Replace("\r", "")
-                                                                   .Replace("\n", "")))
-               {
-                  try
+                  if (baseName == "INotifyPropertyChanged")
                   {
-                     ParseResult parseResult = ModelAttribute.Parse(modelRoot, source);
+                     newClass.ImplementNotify = true;
 
-                     if (parseResult == null)
-                     {
-                        WarningDisplay.Show($"Could not parse '{source}'. The line will be discarded.");
-
-                        continue;
-                     }
-
-                     string message = null;
-
-                     if (string.IsNullOrEmpty(parseResult.Name) || !CodeGenerator.IsValidLanguageIndependentIdentifier(parseResult.Name))
-                        message = $"Could not add '{parseResult.Name}' to {className}: '{parseResult.Name}' is not a valid .NET identifier";
-                     else if (newClass.AllAttributes.Any(x => x.Name == parseResult.Name))
-                        message = $"Could not add {parseResult.Name} to {className}: {parseResult.Name} already in use";
-                     else if (newClass.AllNavigationProperties().Any(p => p.PropertyName == parseResult.Name))
-                        message = $"Could not add {parseResult.Name} to {className}: {parseResult.Name} already in use";
-
-                     if (message != null)
-                     {
-                        WarningDisplay.Show(message);
-                        continue;
-                     }
-
-                     ModelAttribute modelAttribute = new ModelAttribute(store, new PropertyAssignment(ModelAttribute.NameDomainPropertyId, parseResult.Name), new PropertyAssignment(ModelAttribute.TypeDomainPropertyId, parseResult.Type ?? "String"), new PropertyAssignment(ModelAttribute.RequiredDomainPropertyId, parseResult.Required ?? true), new PropertyAssignment(ModelAttribute.MaxLengthDomainPropertyId, parseResult.MaxLength ?? 0), new PropertyAssignment(ModelAttribute.InitialValueDomainPropertyId, parseResult.InitialValue), new PropertyAssignment(ModelAttribute.IsIdentityDomainPropertyId, parseResult.IsIdentity), new PropertyAssignment(ModelAttribute.SetterVisibilityDomainPropertyId, parseResult.SetterVisibility ?? SetterAccessModifier.Public));
-                     newClass.Attributes.Add(modelAttribute);
+                     continue;
                   }
-                  catch
+
+                  // if we see the base class in the model, it's a superclass. Otherwise, it's a custom interface
+                  ModelClass superClass = modelRoot.Types.OfType<ModelClass>().FirstOrDefault(c => c.Name == baseName);
+
+                  if (superClass != null)
+                     newClass.Superclass = superClass;
+                  else
+                     customInterfaces.Add(baseName);
+               }
+
+               newClass.CustomInterfaces = customInterfaces.Any()
+                                              ? string.Join(",", customInterfaces)
+                                              : null;
+            }
+
+            #endregion Base classes and interfaces
+
+            #region Properties
+
+            newClass.Attributes.Clear();
+
+            foreach (string source in cls.Members.OfType<PropertyDeclarationSyntax>().Select(prop => prop.ToString()).Select(source => Regex.Replace(source, @"\[[^]]+\]", "", RegexOptions.Multiline).Replace("\r", "").Replace("\n", "")))
+            {
+               try
+               {
+                  ParseResult parseResult = ModelAttribute.Parse(modelRoot, source);
+
+                  if (parseResult == null)
                   {
                      WarningDisplay.Show($"Could not parse '{source}'. The line will be discarded.");
+
+                     continue;
                   }
+
+                  string message = null;
+
+                  if (string.IsNullOrEmpty(parseResult.Name) || !CodeGenerator.IsValidLanguageIndependentIdentifier(parseResult.Name))
+                     message = $"Could not add '{parseResult.Name}' to {className}: '{parseResult.Name}' is not a valid .NET identifier";
+                  else if (newClass.AllAttributes.Any(x => x.Name == parseResult.Name))
+                     message = $"Could not add {parseResult.Name} to {className}: {parseResult.Name} already in use";
+                  else if (newClass.AllNavigationProperties().Any(p => p.PropertyName == parseResult.Name))
+                     message = $"Could not add {parseResult.Name} to {className}: {parseResult.Name} already in use";
+
+                  if (message != null)
+                  {
+                     WarningDisplay.Show(message);
+
+                     continue;
+                  }
+
+                  ModelAttribute modelAttribute = new ModelAttribute(store, new PropertyAssignment(ModelAttribute.NameDomainPropertyId, parseResult.Name), new PropertyAssignment(ModelAttribute.TypeDomainPropertyId, parseResult.Type ?? "String"), new PropertyAssignment(ModelAttribute.RequiredDomainPropertyId, parseResult.Required ?? true), new PropertyAssignment(ModelAttribute.MaxLengthDomainPropertyId, parseResult.MaxLength ?? 0), new PropertyAssignment(ModelAttribute.InitialValueDomainPropertyId, parseResult.InitialValue), new PropertyAssignment(ModelAttribute.IsIdentityDomainPropertyId, parseResult.IsIdentity), new PropertyAssignment(ModelAttribute.SetterVisibilityDomainPropertyId, parseResult.SetterVisibility ?? SetterAccessModifier.Public));
+                  newClass.Attributes.Add(modelAttribute);
                }
-            
-               tx.Commit();
+               catch
+               {
+                  WarningDisplay.Show($"Could not parse '{source}'. The line will be discarded.");
+               }
             }
+
+            #endregion Properties
+
+            tx.Commit();
          }
       }
    }
