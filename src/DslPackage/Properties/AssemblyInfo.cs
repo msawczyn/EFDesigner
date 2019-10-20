3ad8e380f4da4e432799085c25ef62959c831274
Michael Sawczyn <michael.sawczyn@awh.net>
2019-10-20 04:40:10 -0400

    Version labeling

--- src/DslPackage/Properties/AssemblyInfo.cs
+++ src/DslPackage/Properties/AssemblyInfo.cs
@@ -21,7 +21,7 @@ using System.Runtime.ConstrainedExecution;
 [assembly: AssemblyCulture("")]
 [assembly: System.Resources.NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("1.3.0.7")]
+[assembly: AssemblyVersion("1.3.0.8")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
 [assembly: ReliabilityContract(Consistency.MayCorruptProcess, Cer.None)]
--- src/Utilities/EFCoreParser/Properties/AssemblyInfo.cs
+++ src/Utilities/EFCoreParser/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.0.7")]
-[assembly: AssemblyFileVersion("1.3.0.7")]
+[assembly: AssemblyVersion("1.3.0.8")]
+[assembly: AssemblyFileVersion("1.3.0.8")]
 [assembly: ReliabilityContract(Consistency.MayCorruptProcess, Cer.None)]
