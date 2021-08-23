38c5206ffecca0cd7cab1034d2011d8dc72965d3
Michael Sawczyn <michaelsawczyn@gmail.com>
2021-08-23 09:29:51 -0400

    v3.0.7.1

--- src/Dsl/GeneratedCode/DomainClasses.cs
+++ src/Dsl/GeneratedCode/DomainClasses.cs
@@ -13526,6 +13526,6 @@ namespace Sawczyn.EFDesigner.EFModel
    /// </summary>
 	partial class ModelRoot
 	{
-		public const string DSLVersion = "3.0.7.0";
+		public const string DSLVersion = "3.0.7.1";
 	}
 }
