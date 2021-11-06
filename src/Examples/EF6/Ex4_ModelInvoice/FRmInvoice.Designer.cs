bcc70747e0f518dc33c915f41f4dbeca2a43b2fc
David <david@digitalgeo.com.au>
2021-11-06 21:32:49 +0800

    Cannot figure out how dto do child tables?

---
+++ src/Examples/EF6/Ex4_ModelInvoice/FRmInvoice.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace Ex4_ModelInvoice
+{
+    partial class FRmInvoice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInvoice = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInvoice
+            // 
+            this.btnInvoice.Location = new System.Drawing.Point(60, 169);
+            this.btnInvoice.Name = "btnInvoice";
+            this.btnInvoice.Size = new System.Drawing.Size(192, 53);
+            this.btnInvoice.TabIndex = 0;
+            this.btnInvoice.Text = "Invoice";
+            this.btnInvoice.UseVisualStyleBackColor = true;
+            this.btnInvoice.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FRmInvoice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnInvoice);
+            this.Name = "FRmInvoice";
+            this.Text = "Accounting Mock up";
+            this.ResumeLayout(false);
+
+        }
+
+      #endregion
+
+      private System.Windows.Forms.Button btnInvoice;
+   }
+}
+
---
+++ src/Examples/EF6/Ex4_ModelInvoice/FRmInvoice.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex4_ModelInvoice
+{
+    public partial class FRmInvoice : Form
+    {
+        public FRmInvoice()
+        {
+            InitializeComponent();
+        }
+
+      private void button1_Click(object sender, EventArgs e)
+      {
+
+         using (AccountingSystemModel db = new AccountingSystemModel())
+         {
+            //Create new, empty entities after running SQL script generated to get model in database
+            var invHeader = db.InvoiceHeaders.Create();
+            var invDetails = db.InvoiceDetails.Create();
+
+            invHeader.Total = 150M;
+            invDetails.ItemDescription = "New Item";
+            invDetails.Price = 75M;
+            invDetails.Quantity = 2;
+
+
+
+            //Trying to do this..as an example..
+            //https://github.com/andern7/EFModelFirst/blob/master/Program.cs
+
+            /
+            invHeader.InvoiceDetails.Add(invDetails);
+            db.InvoiceHeaders.Add(invHeader);
+            db.SaveChanges();
+
+            //This would be good for example 5, ordering system
+            //https://github.com/ionbogatu/EFModelDesignerFirst/tree/master/EFModelDesignerFirst
+         }
+
+      }
+   }
+}
