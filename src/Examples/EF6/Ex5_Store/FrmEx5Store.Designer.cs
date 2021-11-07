ff9039e1094e76b4d28d19a0ed164dfc74b4db39
David <david@digitalgeo.com.au>
2021-11-07 21:24:14 +0800

    To Do
    FrmCourseManager: Line 348
    
    I dont know how to create an enrolment?

--- src/Examples/EF6/Ex5_Store/FrmEx5Store.Designer.cs
+++ src/Examples/EF6/Ex5_Store/FrmEx5Store.Designer.cs
@@ -29,21 +29,35 @@ namespace Ex5_Store
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(251, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "No COde yet..";
             // 
             // FrmEx5Store
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Name = "FrmEx5Store";
             this.Text = "Ex 5 Store";
             this.Load += new System.EventHandler(this.FrmEx5Store_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
-        #endregion
-    }
+      #endregion
+
+      private System.Windows.Forms.Label label1;
+   }
 }
 
